8181176d595e277e31ee54f35cceff2297ef8f13
Shreya Gurram <gurram13775@gmail.com>
2024-01-30 12:49:53 +0530

    adds css

--- superlit-offline-frontend/app/viewClassroom/page.js
+++ superlit-offline-frontend/app/viewClassroom/page.js
@@ -1,20 +1,26 @@
 export default function ViewClassroom() {
- var classArray = [["3-A","28-Jan-2024","02-Feb-2024"],["3-B","28-Jan-2024","02-Feb-2024"],["3-C","28-Jan-2024","02-Feb-2024"],["Class 4","28-Jan-2024","02-Feb-2024"],["Class 5","28-Jan-2024","02-Feb-2024"],["Class 6","28-Jan-2024","02-Feb-2024"],["Class 7","28-Jan-2024","02-Feb-2024"],["Class 8","28-Jan-2024","02-Feb-2024"],["Class 9","28-Jan-2024","02-Feb-2024"],["Class 10","28-Jan-2024","02-Feb-2024"],["Class 11","28-Jan-2024","02-Feb-2024"],["Class 12","28-Jan-2024","02-Feb-2024"]];
+ var classArray = [["3-A","28-Jan-2024","02-Feb-2024"],["3-B","28-Jan-2024","02-Feb-2024"],["3-C","28-Jan-2024","02-Feb-2024"]];
 
  return (
-    <div className="classroom">
-      <div className="m-6 p-5">Add Another Classroom </div>
-      <div className="grid grid-cols-3 m-2 p-2 rounded-xl justify-center items-center">
+    <div className="classroom bg-[#0F0913]">
+      <button className="text-white rounded-full text-sm px-5 py-2.5 text-center mb-2 bg-ring-purple-900 ">Add Another Classroom </button>
+      <div className="grid grid-cols-3 m-24 p-2 rounded-xl justify-center items-center bg-[#0F0913] ">
         {classArray.map((classItem, index) => (
           <div key={index}>
-            <div className="flex flex-col border border-white p-5 m-6 rounded-2xl gap-x-24 gap-y-8 text-2xl bg-primary bg-opacity-5">
-            <div className="flex justify-center items-center text-3xl">{classItem[0]}</div>
-            <div>Assigned On:{classItem[1]}</div>
-            <div>Due On:{classItem[2]}</div>
+            <div className="flex flex-col p-4 justify-center m-16 rounded-2xl text-2xl gap-x-12 gap-y-6"  
+                 style={{ 
+                    background: `linear-gradient(
+      to top,
+      rgba(117, 117, 239, 0.5) 0%,
+      rgba(0, 0, 0, 0.5) 92.3%)`,
+                    backdropFilter: 'blur(5px)'
+                 }}>
+            <div className="flex justify-center items-center">{classItem[0]}</div>
+            <div className="text-base ml-8">Assigned On:{classItem[1]}</div>
+            <div className="text-base ml-8 mb-4">Due On:{classItem[2]}</div>
           </div>
           </div>
         ))}
-        <div className="flex flex-col justify-center items-center border border-white p-5 m-6 text-2xl">Class1<div className=" text-lg">Assignment Set: </div><div className=" text-lg">Due On: </div><div className=" text-lg">No. of submissions: </div></div>
       </div>
     </div>
  );
