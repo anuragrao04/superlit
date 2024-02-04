2391acbe751879a9f43ad959c58c3d5382af2e31
Shreya Gurram <gurram13775@gmail.com>
2024-02-04 16:39:30 +0530

    adds create classroom button that doesnt work

--- superlit-offline-frontend/app/viewClassroom/page.js
+++ superlit-offline-frontend/app/viewClassroom/page.js
@@ -1,12 +1,12 @@
 import './style.css';
 export default function ViewClassroom() {
- var classArray = [["3-A","Week-2","28-Jan-2024","02-Feb-2024"],["3-B","Week-4","28-Jan-2024","02-Feb-2024"],["3-C","Week-5","28-Jan-2024","02-Feb-2024"],["3-D","Week-3","01-Feb-2024","02-Feb-2024"],["3-E","Week-6","03-Feb-2024","04-Feb-2024"],["3-F","Week-7","03-Feb-2024","04-Feb-2024"],["3-G","Week-8","03-Feb-2024","04-Feb-2024"]];
+ var classArray = [["3-A","Week-2","28-Jan-2024","02-Feb-2024"],["3-B","Week-4","28-Jan-2024","02-Feb-2024"]]
 
  return (
  <div>
  <div className="container">
-        <div className="text-6xl m-12">Your Classrooms</div>
-        <div className=" grid grid-cols-3 gap-12">
+        <div className="lg:text-6xl md:text-4xl text-3xl m-12">Your Classrooms</div>
+        <div className=" grid lg:grid-cols-3 md:grid-cols-2 gap-12">
           {classArray.map((classItem, index) => (
             <div key={index}>
               <div className="card">
@@ -21,8 +21,15 @@ export default function ViewClassroom() {
               </div>
             </div>
           ))}
-        </div>
-      </div>  
+              <div className="card">
+                <div className="border"></div>
+                <div className="filter"></div>
+                <div className="text-6xl">+</div>
+                <div className="shadow"></div>
+                <div className="backdrop"></div>
+              </div>
+      </div> 
+      </div>
 <div className="circle one"></div>
 <div className="circle two"></div>
 <svg>
