f1997c1b98e66cba83fec9e5adaa843f0a0c104c
Shreya Gurram <gurram13775@gmail.com>
2024-04-01 10:58:14 +0530

    changes to sd

--- superlit-offline-frontend/.next/server/flight-server-css-manifest.js
+++ superlit-offline-frontend/.next/server/flight-server-css-manifest.js
@@ -1,16 +1,30 @@
 self.__RSC_CSS_MANIFEST={
   "cssImports": {
+    "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/viewClassroom/page.js": [
+      "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/viewClassroom/style.css"
+    ],
     "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/layout.js": [
       "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/globals.css"
     ],
     "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/studentDash/page.js": [
       "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/studentDash/style.css"
+    ],
+    "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/tD/page.js": [
+      "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/tD/style.css"
     ]
   },
   "cssModules": {
+    "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/viewClassroom/page": [
+      "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/globals.css",
+      "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/viewClassroom/style.css"
+    ],
     "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/studentDash/page": [
       "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/globals.css",
       "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/studentDash/style.css"
+    ],
+    "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/tD/page": [
+      "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/tD/style.css",
+      "/home/shreya/Documents/Stuff/Projects-Repos/superlit/superlit-offline-frontend/app/globals.css"
     ]
   }
 }
